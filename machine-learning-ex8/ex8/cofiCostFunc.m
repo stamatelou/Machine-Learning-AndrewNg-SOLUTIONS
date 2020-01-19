5776ed39d5e75fedc33b56c1fee1e7a5432c0813
Elena <elenistamatelou@gmail.com>
2020-01-19 17:42:34 +0100

    Add files via upload

---
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -0,0 +1,79 @@
+function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
+                                  num_features, lambda)
+%COFICOSTFUNC Collaborative filtering cost function
+%   [J, grad] = COFICOSTFUNC(params, Y, R, num_users, num_movies, ...
+%   num_features, lambda) returns the cost and gradient for the
+%   collaborative filtering problem.
+%
+
+% Unfold the U and W matrices from params
+X = reshape(params(1:num_movies*num_features), num_movies, num_features);
+Theta = reshape(params(num_movies*num_features+1:end), ...
+                num_users, num_features);
+
+# Cost Function
+M = X * Theta';
+rat = R.* M;
+diff = (rat - Y).^2;
+J = sum(sum(diff))/2;
+
+term = lambda / 2;
+reg1 = term * sum(sum(Theta.^2));
+reg2 = term * sum(sum(X.^2));
+J = J + reg1 + reg2;
+ 
+
+# Gradient function
+X_grad = zeros(size(X));
+Theta_grad = zeros(size(Theta));
+
+
+difference = (rat - Y);
+X_grad = difference * Theta;
+Theta_grad = difference' * X;
+
+X_grad = X_grad + lambda * X;
+Theta_grad = Theta_grad + lambda * Theta;
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost function and gradient for collaborative
+%               filtering. Concretely, you should first implement the cost
+%               function (without regularization) and make sure it is
+%               matches our costs. After that, you should implement the 
+%               gradient and use the checkCostFunction routine to check
+%               that the gradient is correct. Finally, you should implement
+%               regularization.
+%
+% Notes: X - num_movies  x num_features matrix of movie features
+%        Theta - num_users  x num_features matrix of user features
+%        Y - num_movies x num_users matrix of user ratings of movies
+%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+%            i-th movie was rated by the j-th user
+%
+% You should set the following variables correctly:
+%
+%        X_grad - num_movies x num_features matrix, containing the 
+%                 partial derivatives w.r.t. to each element of X
+%        Theta_grad - num_users x num_features matrix, containing the 
+%                     partial derivatives w.r.t. to each element of Theta
+%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% =============================================================
+
+grad = [X_grad(:); Theta_grad(:)];
+
+end
